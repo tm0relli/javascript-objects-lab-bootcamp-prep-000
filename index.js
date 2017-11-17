eff322b4e7603d5c28d75e428c2baa332d30ecac
tm0relli <theresa.l.morelli@gmail.com>
2017-11-17 22:53:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 var recipes = {};
-//returns an object with the orignal key value pairs and the new key value pair
+// returns an object with the orignal key value pairs and the new key value pair
 function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, object, { [key]: value })
+}
+
+// 1) returns object without the delete key/value pair
+// 2) modifies the original object
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return object;
 }
